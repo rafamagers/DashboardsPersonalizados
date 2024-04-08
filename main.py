21c92ed6782d6907f900f24580ba7c162aa2c4fe
Rafael Angel Martinez Montero <ramontero@uninorte.edu.co>
2024-04-07 23:31:47 -0500

    Analisis factorial

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import dash
-from dash import dcc, html, Input, Output, State
+from dash import dcc, html, Input, Output, State, dash_table
 import pandas as pd
 import plotly.express as px
 import io
+import plotly.graph_objs as go
+import numpy as np
 import base64
 from dash.exceptions import PreventUpdate
 from ydata_profiling import ProfileReport
+from factor_analyzer import FactorAnalyzer
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
 import webbrowser
 import dash_bootstrap_components as dbc
 external_stylesheets = ['assets/styles.css']
@@ -14,6 +20,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 # Inicializar df como un DataFrame vacío
 
 df = pd.DataFrame()
+filtered_df = pd.DataFrame()
 
 
 # Diseño de la aplicación
@@ -162,6 +169,9 @@ app.layout = html.Div([
     
     # Gráfico de Barras Agrupadas (Bivariado)
     dcc.Graph(id='grouped-bar-bi-plot'),
+
+
+
     dcc.Dropdown(
         id='dropdown-column',
         options=[{'label': col, 'value': col} for col in df.columns],
@@ -175,7 +185,27 @@ app.layout = html.Div([
 
 
 
-    html.Button("Abrir Informe", id="abrir-informe-button")
+    html.Button("Abrir Informe", id="abrir-informe-button"),
+    html.H1("Visualizar y seleccionar columnas"),
+    
+    # Checklist para seleccionar columnas
+    html.Div([
+        dcc.Checklist(
+            id='column-selector',
+            options=[{'label': col, 'value': col} for col in df.columns],
+            value=df.columns.tolist(),  # Seleccionar todas las columnas por defecto
+            inline=True
+        ),
+    ]),
+    html.Div(id='nada'),
+    html.H1('Visualización de Análisis Factorial Exploratorio'),
+    dcc.Input(id='input-number', type='number', value=3, min=1, max=10, step=1),
+    html.Button("Realizar análisis factorial", id="analisisfactorial"),
+    html.Div([
+        dcc.Graph(id='factor_loading_plot'),
+        dcc.Graph(id='eigenvalue_plot'),
+        dcc.Graph(id='explained_variance_plot')
+    ])
 
 
 ])
@@ -204,6 +234,7 @@ def parse_contents(contents):
                Output('3d-x-dropdown', 'options'),
                Output('3d-y-dropdown', 'options'),
                Output('3d-z-dropdown', 'options'),
+               Output('column-selector', 'options'),
                Output('grouped-bar-x-dropdown', 'options'),
                Output('dropdown-column', 'options'),
                Output('grouped-bar-subgroup-dropdown', 'options')],
@@ -213,7 +244,7 @@ def update_output(contents):
         raise PreventUpdate
     else:
         options = parse_contents(contents)
-        return [html.P(f'Se ha cargado un archivo con {len(df)} filas y {len(df.columns)} columnas.')], options, options, options, options, options, options, options, options,options, options, options, options, options
+        return [html.P(f'Se ha cargado un archivo con {len(df)} filas y {len(df.columns)} columnas.')], options, options,options, options, options, options, options, options, options,options, options, options, options, options
 
 # Callback para actualizar el gráfico dinámico
 @app.callback(
@@ -300,7 +331,7 @@ def update_grouped_bar_bi_plot(selected_x, selected_subgroup):
 )
 def abrir_informe(n_clicks):
     if n_clicks:
-        profile = ProfileReport(df, title="Pandas Profiling Report")
+        profile = ProfileReport(filtered_df, title="Pandas Profiling Report")
         profile.to_file("tu_informe.html")
         # Abre el informe en una nueva ventana o pestaña
         webbrowser.open_new_tab("tu_informe.html")
@@ -324,7 +355,7 @@ def generate_table(column):
     [Input('dropdown-column', 'value')]
 )
 def update_table(column):
-    if column is None:
+    if column == "":
         return []
     else:
         return generate_table(column)
@@ -340,21 +371,127 @@ def update_table(column):
 def update_df(n_clicks,columna, children):
     
     if n_clicks > 0:
-        print(df)
         updated_df = df.copy()  # Copia del DataFrame para no modificar el original
         for child in children:
-            print(child)
-            print("gee")
-          
             input = child["props"]["children"][0]["props"]["children"]  # Obtener el ID del input
             updated_df.loc[updated_df[columna] == input, columna] = int(child["props"]["children"][1]["props"]["value"])
         # Actualizar el DataFrame original con los cambios
         df.update(updated_df)
-        print(df)
+        df[columna] = df[columna].astype(int)
         return 'Cambios confirmados y DataFrame actualizado.'
     else:
-        return ''
+        return 'No se ha podido actualizar el dataframe (Recuerde que esto es para codificar)'
 
+
+# Callback para actualizar el DataFrame filtrado
+@app.callback(
+    Output("nada", 'children'),
+    [Input('column-selector', 'value')]
+)
+def update_filtered_data(selected_columns):
+    global filtered_df
+    filtered_df = df[selected_columns]
+    return filtered_df.to_dict('records')
+
+def generate_plots(loadings, eigenvalues, explained_variance):
+    # Gráfico de cargas factoriales
+    plt.figure(figsize=(10, 6))
+    sns.heatmap(loadings, annot=True, cmap='coolwarm', fmt='.2f', cbar=False)
+    plt.title('Gráfico de Cargas Factoriales')
+    plt.xlabel('Factores')
+    plt.ylabel('Variables')
+    factor_loading_plot = plt.gcf()
+
+    # Gráfico de valores propios
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, len(eigenvalues)+1), eigenvalues, marker='o', linestyle='--')
+    plt.title('Gráfico de Valores Propios')
+    plt.xlabel('Número de Factor')
+    plt.ylabel('Valor Propio')
+    plt.grid(True)
+    eigenvalue_plot = plt.gcf()
+
+    # Gráfico de varianza explicada
+    plt.figure(figsize=(8, 6))
+    plt.plot(range(1, len(explained_variance[0])+1), explained_variance[0], marker='o', linestyle='-')
+    plt.title('Gráfico de Varianza Explicada')
+    plt.xlabel('Número de Factores')
+    plt.ylabel('Varianza Explicada')
+    plt.grid(True)
+    explained_variance_plot = plt.gcf()
+
+    return factor_loading_plot, eigenvalue_plot, explained_variance_plot
+
+
+@app.callback(
+    [Output('factor_loading_plot', 'figure'),
+     Output('eigenvalue_plot', 'figure'),
+     Output('explained_variance_plot', 'figure')
+     ],
+    [Input('analisisfactorial', 'n_clicks')],
+    [State('input-number', 'value')]
+  # Aquí debes especificar el elemento de entrada que desencadena la actualización de los gráficos
+)
+def update_plots(n_clicks,input_value):
+    # Aquí deberías realizar el análisis factorial exploratorio y obtener los resultados
+    # Luego, llamar a la función para generar los gráficos
+    if n_clicks:
+        if filtered_df.empty:
+            raise PreventUpdate
+        else:
+            scaler = StandardScaler()
+            
+            data_scaled = scaler.fit_transform(filtered_df)
+            correlation_matrix = filtered_df.corr()
+            fa = FactorAnalyzer(n_factors=input_value, rotation=None) 
+            fa.fit(data_scaled)
+            print(fa.sufficiency)
+            # Obtener valores propios
+            eigenvalues = fa.get_eigenvalues()
+            print(eigenvalues)
+            # Obtener cargas factoriales
+            loadings = fa.loadings_
+            print(loadings)
+            # Obtener varianza explicada
+            explained_variance = fa.get_factor_variance()
+            print(explained_variance)
+            # Crear un gráfico de líneas para la varianza explicada
+            # Crear el heatmap
+            heatmap_trace = go.Heatmap(z=correlation_matrix.values,
+                                    x=correlation_matrix.columns,
+                                    y=correlation_matrix.index,
+                                    colorscale='Viridis',
+                                    colorbar=dict(title='Correlation'))
+
+            heatmap_layout = go.Layout(title='Correlation Matrix',
+                                    xaxis=dict(title='Variables'),
+                                    yaxis=dict(title='Variables'))
+
+            heatmap_fig = go.Figure(data=[heatmap_trace], layout=heatmap_layout)
+
+            # Gráfico de cargas factoriales
+            loadings_trace = go.Heatmap(z=loadings.T,  # Transponer la matriz de cargas factoriales
+                                        x=filtered_df.columns,
+                                        y=list(range(1, loadings.shape[1] + 1)),  # Asegurarse de que el eje y tenga el tamaño correcto
+                                        colorscale='Viridis',
+                                        colorbar=dict(title='Loading'))
+            # Crear figuras
+            loadings_fig = go.Figure(data=[loadings_trace], layout=dict(title='Factor Loadings Heatmap', xaxis=dict(title='Variables'), yaxis=dict(title='Factor Number')))
+            bar_traces = []
+            for factor_num in range(loadings.shape[1]):  # Iterar sobre el segundo eje de 'loadings'
+                bar_trace = go.Bar(x=filtered_df.columns, y=loadings[:, factor_num], name=f'Factor {factor_num+1}')
+                bar_traces.append(bar_trace)
+
+            bar_fig = go.Figure(data=bar_traces, layout=dict(title='Factor Loadings per Variable', xaxis=dict(title='Variable'), yaxis=dict(title='Loading')))
+
+
+            # Retornar las figuras en tu callback
+            return heatmap_fig, loadings_fig, bar_fig
+    else:
+        # Si no se ha presionado el botón, no se genera ningún gráfico
+        raise PreventUpdate
+
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
 
