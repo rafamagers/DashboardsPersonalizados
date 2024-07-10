26b009b423fa0f8e4ec856d4aa645234b321d5ef
Rafael Angel Martinez Montero <ramontero@uninorte.edu.co>
2024-07-09 23:47:53 -0500

    adding graphs

--- pruebas2.py
+++ pruebas2.py
@@ -1,38 +1,19 @@
-import pandas as pd
-from factor_analyzer import calculate_kmo, calculate_bartlett_sphericity
-from scipy.stats import bartlett
+import os
+import streamlit as st
+import semopy
 
-def realizar_tests(ruta_archivo):
-    # Leer el archivo CSV
-    datos = pd.read_csv(ruta_archivo)
-    
-    # Seleccionar las columnas a partir de la cuarta columna
-    datos_seleccionados = datos.iloc[:, 3:]
-    
-    # Test de Bartlett
-    chi_square_value, p_value = calculate_bartlett_sphericity(datos_seleccionados)
-    
-    # Test KMO
-    kmo_all, kmo_model = calculate_kmo(datos_seleccionados)
-    
-    # Resultados
-    resultados = {
-        'bartlett': {
-            'chi_square_value': chi_square_value,
-            'p_value': p_value
-        },
-        'kmo': kmo_model
-    }
-    
-    return resultados
+# Añadir la ruta de Graphviz al PATH
+os.environ["PATH"] += os.pathsep + '/usr/local/bin'  # Reemplaza con la ruta correcta si es necesario
 
-# Ejemplo de uso
-ruta_csv = "Z:\Downloads\Ejercicio_Escala-de-Cordialidad_Base-de-Datos.csv"
-resultados = realizar_tests(ruta_csv)
+# Tu código Streamlit y Semopy
+def main():
+    data = semopy.examples.political_democracy.get_data()
+    mod = semopy.examples.political_democracy.get_model()
+    m = semopy.Model(mod)
+    m.fit(data)
+    g = semopy.semplot(m, "pd.png")
 
-# Imprimir los resultados
-print("Test de Bartlett:")
-print("Chi-Square Value:", resultados['bartlett']['chi_square_value'])
-print("P-Value:", resultados['bartlett']['p_value'])
-print("\nTest KMO:")
-print("KMO Model:", resultados['kmo'])
+    st.image("pd.png")
+
+if __name__ == "__main__":
+    main()
