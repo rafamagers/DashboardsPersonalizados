ba228b3b38b93c1acf63145a6abc3f4f1438d325
Rafael Angel Martinez Montero <ramontero@uninorte.edu.co>
2024-07-16 10:48:27 -0500

    adding R

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Descargar y compilar R en el directorio del usuario
-mkdir -p ~/R
-cd ~/R
+# Directorio de instalación de R
+R_DIR=$HOME/R
+
+# Crear el directorio si no existe
+mkdir -p $R_DIR
+cd $R_DIR
+
+# Descargar y compilar R
 wget https://cran.r-project.org/src/base/R-4/R-4.0.2.tar.gz
 tar -xzvf R-4.0.2.tar.gz
 cd R-4.0.2
 
 # Configurar e instalar R en el directorio del usuario
-./configure --prefix=$HOME/R
+./configure --prefix=$R_DIR
 make
 make install
 
-# Añadir R a la variable PATH
+# Añadir R a la variable PATH y definir R_HOME
 echo 'export PATH=$HOME/R/bin:$PATH' >> ~/.bashrc
 echo 'export R_HOME=$HOME/R' >> ~/.bashrc
 
@@ -21,3 +26,9 @@ source ~/.bashrc
 
 # Confirmar que R está instalado
 R --version
+
+# Instalar las dependencias de Python
+pip3 install --user streamlit rpy2 RyStats pandas
+
+# Confirmar que las dependencias están instaladas
+pip3 list | grep -E "streamlit|rpy2|RyStats|pandas"
